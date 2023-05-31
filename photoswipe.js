d2f4c549d62d17a1283b2b8b1167747fbfe6065e
vidursaigal <vsaigal3@seas.upenn.edu>
2023-05-31 18:59:57 -0400

    updates

---
+++ photoswipe.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* global jQuery, PhotoSwipe, PhotoSwipeUI_Default, console */
+
+(function($) {
+
+  // Init empty gallery array
+  var container = [];
+
+  // Loop over gallery items and push it to the array
+  $('#gallery').find('figure').each(function() {
+    var $link = $(this).find('a'),
+      item = {
+        src: $link.attr('href'),
+        w: $link.data('width'),
+        h: $link.data('height'),
+        title: $link.data('caption')
+      };
+    container.push(item);
+  });
+
+  // Define click event on gallery item
+  $('a').click(function(event) {
+
+    // Prevent location change
+    event.preventDefault();
+
+    // Define object and gallery options
+    var $pswp = $('.pswp')[0],
+      options = {
+        index: $(this).parent('figure').index(),
+    bgOpacity: 0.8,
+    captionEl: false,
+    tapToClose: true,
+    shareEl: false,
+    fullscreenEl: false,
+      };
+
+    // Initialize PhotoSwipe
+    var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, container, options);
+    gallery.init();
+  });
+
+}(jQuery));
